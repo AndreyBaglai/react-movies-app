35aae289cfaf6e376bfaa0cf1b7be69885641d01
Andrey <36437353+AndreyBaglai@users.noreply.github.com>
2020-11-06 12:00:30 +0200

    Get movies from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Movie from'./components/Movie';
+
+const API_KEY = 'f29cbeafe556227a999c9277467da814';
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const API_IMG = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH = `https://api.themoviedb.org/3/search?&api_key=${API_KEY}`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL).then(res => res.json()).then(data => {
+      console.log(data);
+      setMovies(data.results);
+    });
+  }, [])
+
   return (
     <div className="App">
-      <h1>Init files</h1>
+      {movies.length && movies.map(movie => {
+        return <Movie />
+      })}
     </div>
   );
 }
